899158e0f43535222b06a7a521d4ff95cc70fb79
AlbanePixium <albane.gril@pixiumdigital.com>
2020-09-03 10:30:30 +0800

    :zap: change(form) add onsubmit event to save compileJson

--- src/FormBuilder.php
+++ src/FormBuilder.php
@@ -1,11 +1,8 @@
 <?php
 namespace pixium\form_builder;
 
-use yii\base\Widget;
 use yii\widgets\InputWidget;
 use yii\helpers\Inflector;
-
-use Yii;
 use yii\helpers\Html;
 
 /**
@@ -37,15 +34,7 @@ class FormBuilder extends InputWidget
     /*
     Default json form file
     */
-    public $defaultData = '{
-                                "sections": [
-                                    {
-                                        "name": "",
-                                        "blocks": [
-                                        ]
-                                    }
-                                ]
-                            }';
+    public $defaultData = '{"sections": [{"name": "", "blocks": []}]}';
 
     /*
     To switch between the modes ['run', 'build'] of the FormBuilder
@@ -58,11 +47,6 @@ class FormBuilder extends InputWidget
      */
     public $containerOptions = [];
 
-    // /*
-    // Id for the result hidden input field
-    // */
-    // public $hiddenInputId = 'hidden-input-json';
-
    /**
      * {@inheritdoc}
      */
@@ -73,17 +57,9 @@ class FormBuilder extends InputWidget
             $this->containerOptions['id'] = $this->options['id'] . '-form-builder';
         }
 
-        // if($this->div){
-        //     $div = $this->div;
-        // }
-
         if(!$this->data){
             $this->data = $this->defaultData;
         }
-
-        // if($this->mode){
-        //     $mode = (string) $this->mode;
-        // }
     }
 
     /**
@@ -92,41 +68,30 @@ class FormBuilder extends InputWidget
     public function run()
     {
         // parent::run();
-        echo Html::button('SAVE QUESTIONS', ['id' => 'form-builder-save-btn']);
+        //Create temporary external save btn with same id as interne save btn
+        // echo Html::button('SAVE QUESTIONS', ['id' => 'form-builder-save-btn']);
 
         $this->registerClientScript();
         if ($this->hasModel()) {
             $this->options['value'] = $this->data; 
-            // echo Html::activeHiddenInput($this->model, $this->attribute, $this->options);
-            echo Html::activeTextInput($this->model, $this->attribute, $this->options);
+            echo Html::activeHiddenInput($this->model, $this->attribute, $this->options);
         } else {
-            // echo Html::hiddenInput($this->name, $this->value, $this->options);
-            echo Html::textInput($this->name, $this->value, $this->options);
+            echo Html::hiddenInput($this->name, $this->value, $this->options);
         }
         echo Html::tag('div', '', $this->containerOptions);
     }
 
-        /**
+    /**
      * Registers the needed client script.
      */
     public function registerClientScript()
     {
         $view = $this->getView();
         FormBuilderAssets::register($view);
-        // FormBuilderAsset::register($view);    
 
         $hiddenInputId = $this->options['id'];
         $formBuilderName = Inflector::variablize($hiddenInputId) . 'FormBuilder_' . hash('crc32', $hiddenInputId);
-        // $this->options['data-form-build-name'] = $formBuilderName;
-        // $jsUpdateHiddenField = "document.getElementById('$hiddenInputId').value = $formBuilderName.compileJson() ;";
-        $jsUpdateHiddenField = "jQuery('#$hiddenInputId').val($formBuilderName.compileJson());";
-
-        // if (isset($this->clientOptions['onChange'])) {
-        //     $userFunction = " var userFunction = {$this->clientOptions['onChange']}; userFunction.call(this);";
-        // } else {
-        //     $userFunction = '';
-        // }
-        // $this->clientOptions['onChange'] = new JsExpression("function() {{$jsUpdateHiddenField}$userFunction}");
+        $jsUpdateHiddenField = "document.getElementById('$hiddenInputId').value = $formBuilderName.compileJson() ;";
 
         $jsCode = 
         // Init From builder Object
@@ -138,13 +103,9 @@ class FormBuilder extends InputWidget
         // Run Instance
         ."$formBuilderName.run();\n"
         // Set form submit to trigger jsonComplie
-        // ."jQuery('#$hiddenInputId').parents('form').submit(function() { console.log($formBuilderName.compileJson()); {$jsUpdateHiddenField}});"
-        // ."document.getElementById('$hiddenInputId').parentNode.closest('form').addEventListener('submit', (event) => {{$jsUpdateHiddenField}});"
-        ."document.getElementById('form-builder-save-btn').onclick = function() {
-            var textnode = document.createTextNode(".$formBuilderName.".compileJson()); 
-            console.log(textnode);
-            document.getElementById('".$this->containerOptions['id']."').appendChild(textnode);      
-          }"
+        ."document.getElementById('$hiddenInputId').parentNode.closest('form').addEventListener('submit', function(){{$jsUpdateHiddenField}});"
+        // add onclick event to save btn
+        // ."document.getElementById('form-builder-save-btn').onclick = function() {$jsUpdateHiddenField}"
         ;
 
         // Add js to view 
