c9cd13ec1aa3b33d20fd451c631b68919906a1c2
AlbanePixium <albane.gril@pixiumdigital.com>
2020-09-09 15:16:49 +0800

    :sparkles: feat(assets) include assets files from gitbub library

--- src/FormBuilderAssets.php
+++ src/FormBuilderAssets.php
@@ -6,10 +6,11 @@ use yii\web\AssetBundle;
 class FormBuilderAssets extends AssetBundle
 {
     // public $sourcePath = '@app/widgets/form-builder/assets'; //WIP: replace by npm call 
-    public $css = ['css/main.css'];
+    public $css = [
+        'https://pixiumdigital.github.io/library/form-builder/dist/main.css'
+    ];
     public $js = [
-        // 'js/bundle.js',
-        'https://raw.githubusercontent.com/pixiumdigital/form-builder/master/dist/bundle.js?token=AOYV6WXQFLRHDBBY373JMWC7MGBTC'
+        'https://pixiumdigital.github.io/library/form-builder/dist/bundle.js'
     ];
 
     public function init()
